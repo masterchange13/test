6ca6650ed770ad53e07d128db6f06d4dc081e8c1
mao <2042313869@qq/com>
2023-09-17 19:10:52 +0800

    first

---
+++ dsad.cpp
@@ -0,0 +1,28 @@
+// -*- coding:utf-8 _*-
+/*
+@Created by Mao
+@Author:mao
+@Github:https://github.com/masterchange13?tab=projects
+@Gitee:https://gitee.com/master_change13
+ 
+@Time: 2023
+@Motto:不积跬步无以至千里，不积小流无以成江海，程序人生的精彩需要坚持不懈地积累！
+@target: 大厂offer，高年薪
+
+@@ written by GuangZhi Mao
+
+@from:
+@code target:
+*/
+
+#include "iostream"
+#include "vector"
+
+using namespace std;
+
+
+int main (int argc, char *argv[]) {
+    
+    cout << "hello world" << endl;
+    return 0;
+}
